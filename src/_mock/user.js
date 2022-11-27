b548fa41cad7f771c9187c7f35bf8cad516bdaa1
mahdi <mohamadmahdi.ghoussein@gmail.com>
2022-11-28 00:09:33 +0200

    fff

--- src/_mock/user.js
+++ src/_mock/user.js
@@ -6,7 +6,7 @@ import { sample } from 'lodash';
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   avatarUrl: `/static/mock-images/avatars/avatar_${index + 1}.jpg`,
-  name: faker.name.fullName(),
+  name: faker.name.findName(),
   company: faker.company.companyName(),
   isVerified: faker.datatype.boolean(),
   status: sample(['active', 'banned']),
