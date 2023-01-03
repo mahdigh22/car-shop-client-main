be6a653bf653d6b1529b6bbb568dc493154a5355
mahdigh22 <mohamadmahdi.ghoussein@gmail.com>
2023-01-03 22:53:37 +0200

    route

--- src/components/nav-item.js
+++ src/components/nav-item.js
@@ -1,30 +1,36 @@
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
-import { Box, Button, ListItem } from '@mui/material';
+import { Box, Button, ListItem, ListItemButton, styled } from '@mui/material';
+import {  RouterLink} from 'react-router-dom';
 
+const ListItemStyle = styled((props) => <ListItemButton disableGutters {...props} />)(({ theme }) => ({
+  ...theme.typography.body2,
+  height: 48,
+  position: 'relative',
+  textTransform: 'capitalize',
+  color: theme.palette.text.secondary,
+  borderRadius: theme.shape.borderRadius,
+}));
 export const NavItem = (props) => {
   const { href, icon, title, ...others } = props;
   const router = useRouter();
-  const active = href ? (router.pathname === href) : false;
+  const active = href ? router.pathname === href : false;
 
   return (
     <ListItem
       disableGutters
       sx={{
         display: 'flex',
-       
+
         py: 1,
-        px: 2
+        px: 2,
       }}
       {...others}
     >
-      <NextLink
-        href={href}
-        passHref
-      >
+      <ListItemStyle component={RouterLink} to={href}>
         <Button
-          component="a"
+        
           startIcon={icon}
           disableRipple
           sx={{
@@ -32,25 +38,23 @@ export const NavItem = (props) => {
             borderRadius: 1,
             color: active ? 'secondary.main' : 'black',
             fontWeight: active && 'fontWeightBold',
-            fontSize:'15px',
+            fontSize: '15px',
             justifyContent: 'flex-start',
             px: 3,
             textAlign: 'left',
             textTransform: 'none',
             width: '100%',
             '& .MuiButton-startIcon': {
-              color: active ? 'secondary.main' : 'neutral.400'
+              color: active ? 'secondary.main' : 'neutral.400',
             },
             '&:hover': {
-              backgroundColor: 'grey'
-            }
+              backgroundColor: 'grey',
+            },
           }}
         >
-          <Box sx={{ flexGrow: 1 }}>
-            {title}
-          </Box>
+          <Box sx={{ flexGrow: 1 }}>{title}</Box>
         </Button>
-      </NextLink>
+      </ListItemStyle>
     </ListItem>
   );
 };
@@ -58,5 +62,5 @@ export const NavItem = (props) => {
 NavItem.propTypes = {
   href: PropTypes.string,
   icon: PropTypes.node,
-  title: PropTypes.string
+  title: PropTypes.string,
 };
